484338f4476a24ba64c881398ce426a630d6bb23
F4A4 <122023066+F4A4@users.noreply.github.com>
2023-11-13 16:26:41 +0300

    Add files via upload

---
+++ 20121129/BasvuruTurleri/BasvuruTurleri/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BasvuruTurleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Araba bmw1 = new Araba();
+        Araba bmw2;
+        int sayi1 = 37;
+        int sayi2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayi2 = sayi1;
+            MessageBox.Show(String.Format("sayi1 = {0}, sayi2 = {1}", sayi1, sayi2));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (bmw2 == null)
+            {
+                bmw2 = bmw1;
+            }
+            //bmw2 = bmw1;
+            bmw1.kapiSayisi = 4;
+            bmw2.kapiSayisi = 4;
+            MessageBox.Show(String.Format("bmw1 in kapı sayısı = {0}, bmw2 nin kapı sayısı = {1}",bmw1.kapiSayisi.ToString(),bmw2.kapiSayisi.ToString()));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sayi2 = sayi1;
+            sayi1--;
+            sayi2++;
+            MessageBox.Show(String.Format("sayi1 = {0}, sayi2 = {1}", sayi1, sayi2));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bmw2 = bmw1;
+            bmw1.kapiSayisi = 104;
+            MessageBox.Show(String.Format("bmw1 in kapı sayısı = {0}, bmw2 nin kapı sayısı = {1}", bmw1.kapiSayisi.ToString(), bmw2.kapiSayisi.ToString()));
+            bmw2.kapiSayisi = 2;
+            MessageBox.Show(String.Format("bmw1 in kapı sayısı = {0}, bmw2 nin kapı sayısı = {1}", bmw1.kapiSayisi.ToString(), bmw2.kapiSayisi.ToString()));
+        }
+    }
+    public class Araba
+    {
+        public int kapiSayisi;
+    }
+}
---
+++ DegerlerVeBasvurular/DegerlerVeBasvurular/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DegerlerVeBasvurular
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int Sayi1 = 37;
+        int Sayi2;
+        
+        Araba Bmv1 = new Araba();
+        Araba Bmv2 = null;
+        //Araba Bmv2; Buda ayrı bir seçenek
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Sayi2 = Sayi1;
+            MessageBox.Show(String.Format("Sayi 1 = {0}----Sayi 2 = {1}",Sayi1,Sayi2));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bmv2 = Bmv1;
+            Bmv1.KapiSayisi = 4;
+            Bmv2.KapiSayisi = 4;
+            MessageBox.Show(String.Format("Bmv1 Kapı Sayısı = {0}----Bmv2 Kapı Sayısı = {1}",Bmv1.KapiSayisi,Bmv2.KapiSayisi));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Sayi2 = Sayi1;
+            Sayi2++;
+            Sayi1--;
+            MessageBox.Show(String.Format("Sayi 1 = {0}----Sayi 2 = {1}", Sayi1, Sayi2));
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bmv2 = Bmv1;
+            Bmv1.KapiSayisi = 10;
+            Bmv2.KapiSayisi = 2;
+            MessageBox.Show(String.Format("Bmv1 Kapı Sayısı = {0}----Bmv2 Kapı Sayısı = {1}", Bmv1.KapiSayisi, Bmv2.KapiSayisi));
+
+        }
+    }
+    public class Araba
+    {
+        public int KapiSayisi;
+ 
+    }
+}
